d28548a518ea2cc1aa0cde8f5a966eeb6f42af61
krteja44 <47230256+krteja44@users.noreply.github.com>
2020-06-23 15:32:48 -0400

    Added code to load table and added docker file

--- config.py
+++ config.py
@@ -4,15 +4,16 @@ DB_DETAILS = {
     'dev': {
         'SOURCE_DB': {
             'DB_TYPE': 'mysql',
-            'DB_HOST': '104.196.70.78',
+            'DB_HOST': '35.237.228.145',
             'DB_NAME': 'retail_db',
             'DB_USER': os.environ.get('SOURCE_DB_USER'),
             'DB_PASS': os.environ.get('SOURCE_DB_PASS')
         },
+
         'TARGET_DB': {
             'DB_TYPE': 'postgres',
-            'DB_HOST': '104.196.70.78',
-            'DB_NAME': 'retail_dw',
+            'DB_HOST': '35.237.228.145',
+            'DB_NAME': 'retail_db',
             'DB_USER': os.environ.get('TARGET_DB_USER'),
             'DB_PASS': os.environ.get('TARGET_DB_PASS')
         }
